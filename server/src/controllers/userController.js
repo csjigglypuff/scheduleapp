bd822b72aad5ae012ecf10dd45f06aa1f6119acc
aissata <nutzela@gmail.com>
2024-06-11 17:25:02 -0400

    db progress

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -3,35 +3,36 @@ const db = require('../models/user');
 const userController = {};
 
 userController.createTable = (req, res, next) => {
-  console.log("in route")
+  console.log("in create table controller")
   const query = `
     CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
       username VARCHAR,
       password VARCHAR
-    )
-  `;
-
+    )`;
   db.query(query)
     .then(result => {          
       console.log("Table created successfully");
-      next();
+      return next();
     })   
-    .catch(err => next(err)); 
+    .catch(err => next({log:e}));  
 };
 
-// userController.addUsers = (req, res, next) => {
-//   const query = `  
-//   CREATE TABLE [IF NOT EXISTS] user (
-//     id INTEGER,
-//     user VARCHAR
-//   )`;
+userController.addUser = (req, res, next) => {
+  console.log("in add user controller")
+  const { username } = req.body;
+  password = "";
+  const query = `
+    INSERT INTO users (username, password)
+    VALUES ($1, $2)
+    RETURNING *
+  `;
+  db.query(query, [username, password])
+    .then(result => {
+      console.log("User entered successfully");
+      return next();
+     })
+    .catch(err => next(err));
+};
 
-//   db.query(query)
-//     .then(result => {          
-//       console.log('Table created successfully');
-//       next();
-//     })   
-//     .catch(err => next(err));
-// }
 module.exports = userController;
--- server/src/server.js
+++ server/src/server.js
@@ -1,28 +1,29 @@
+
 // "use strict";
 // Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const path = require('path');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const userRouter = require('./routes/userRoute.js')
 // Middleware
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//for testing user controller
-app.use('/user', userRouter);
+//user route test
+const userRouter = require('./routes/userRouter');
+app.use('/api', userRouter);
+
 
 // Routes
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
-// app.post('/api/data', (req, res) => {
-//     // Handle POST request data
-//     res.send(req.body);
-// });
+app.post('/api/data', (req, res) => {
+    // Handle POST request data
+    res.send(req.body);
+});
 // Error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
