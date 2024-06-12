e11cf65ff5a147cfc7c50d3814e88d9ca09cb00e
syjeffkim <107138664+syjeffkim@users.noreply.github.com>
2024-06-12 08:34:04 -0700

    switching

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
 
-// const PG_URI = 'postgres://pinbucrd:mx0LeYsKrgiZFKCb9x-SnRpXyxdOeXRi@ruby.db.elephantsql.com/pinbucrd';
+const PG_URI = 'postgres://pinbucrd:mx0LeYsKrgiZFKCb9x-SnRpXyxdOeXRi@ruby.db.elephantsql.com/pinbucrd';
 
-const PG_URI = 'postgresql://Coding:96818@localhost:5432/mydatabase';
+// const PG_URI = 'postgresql://Coding:96818@localhost:5432/mydatabase';
 
 const pool = new Pool({
 	connectionString: PG_URI,
